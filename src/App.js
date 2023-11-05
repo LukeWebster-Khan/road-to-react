465307bf8bd7b5d78d9e7b7af082c8425b48d7a7
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-11-05 18:14:45 +0000

    feat: removed husky from main packages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import List from 'components/List'
-import ListData from 'data/ListData'
-import 'styles/index.scss'
+import List from "components/List";
+import ListData from "data/ListData";
+import "styles/index.scss";
 
 export const App = () => {
     return (
         <div className="App">
             <List list={ListData} />
         </div>
-    )
-}
+    );
+};
 
-export default App
+export default App;
