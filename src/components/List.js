ae06e261b1a33033ea8e4d4321ae5df4b6ac7eac
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-11-12 21:45:55 +0000

    feat: added Item component, removed css

--- src/components/List.js
+++ src/components/List.js
@@ -1,32 +1,40 @@
 import PropTypes from 'prop-types';
 
+const Item = ({ item }) => {
+    return (
+        <li className="list__item" key={item.objectID}>
+            <div className="list__row">
+                Website:
+                <a href={item.url}>{item.title}</a>
+            </div>
+            <div className="list__row">
+                Author:
+                <span>{item.author}</span>
+            </div>
+            <div className="list__row">
+                Comments:
+                <span>{item.num_comments}</span>
+            </div>
+            <div className="list__row">
+                Points:
+                <span>{item.points}</span>
+            </div>
+        </li>
+    );
+};
+
 export const List = ({ list }) => {
     return (
         <ul className="list__unordered">
             {list.map(function (item) {
-                return (
-                    <li className="list__item" key={item.objectID}>
-                        <div className="list__row">
-                            Website:
-                            <a href={item.url}>{item.title}</a>
-                        </div>
-                        <div className="list__row">
-                            Author:
-                            <span>{item.author}</span>
-                        </div>
-                        <div className="list__row">
-                            Comments:
-                            <span>{item.num_comments}</span>
-                        </div>
-                        <div className="list__row">
-                            Points:
-                            <span>{item.points}</span>
-                        </div>
-                    </li>
-                );
+                return <Item key={item.objectID} item={item} />;
             })}
         </ul>
     );
+};
+
+Item.propTypes = {
+    item: PropTypes.object,
 };
 
 List.propTypes = {
